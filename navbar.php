a891cd2d6a192b26c76f5841a23ec0d6b715de90
Rafal Puk <rpuk@live.esu.edu>
2023-02-24 15:23:16 -0500

    Using php and separating files/pages

---
+++ navbar.php
@@ -0,0 +1,35 @@
+    <!-- Navbar -->
+    <nav class="navbar sticky-top opacity-75 navbar-expand-lg bg-secondary navbar-dark">
+      <div class="container-fluid">
+        <a href="#intro" class="navbar-brand">
+          <span class="fw-bold text-warning">
+            About me
+            <i class="bi bi-emoji-smile"></i>
+          </span>
+        </a>
+    <!-- Mobile screen toggle button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" 
+        aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+    <!-- Navbar links -->
+        <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="aboutMe.php">Welcome Page</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contactMe.php">Contact me</a>
+            </li>
+          </ul>
+          <!--form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form-->
+        </div>
+      </div>
+    </nav>
